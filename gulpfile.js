c39736a832d79f03b4e5048f684acff6b61acd05
appalaszynski <35331661+appalaszynski@users.noreply.github.com>
2018-07-03 19:51:28 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const browsersync = require('browser-sync');
+
+gulp.task('browsersync', () => {
+  browsersync.init({
+    server: {
+      baseDir: './dist',
+    },
+    notify: false,
+  });
+});
+
+gulp.task('scss', () => {
+  return gulp.src('./src/scss/main.scss')
+    .pipe(sass({
+      outputStyle: 'nested',
+    }).on('error', sass.logError))
+    .pipe(rename('style.css'))
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(browsersync.stream());
+});
+
+gulp.task('watch:scss', () => {
+  gulp.watch('./src/**/*.scss', gulp.series('scss'));
+});
+
+gulp.task('watch:html', () => {
+  gulp.watch("./dist/*.html").on('change', browsersync.reload);
+});
+
+gulp.task('dev', gulp.parallel('watch:html', 'watch:scss', 'browsersync'));
